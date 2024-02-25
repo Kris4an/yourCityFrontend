485d85e6734c10bc8ccc4cc3029f1e05558a386d
Kris4an <67221104+Kris4an@users.noreply.github.com>
2024-02-25 21:57:19 +0200

    Fix admin page not being forbidden

---
+++ src/routes/admin/+layout.ts
@@ -0,0 +1,29 @@
+import { error } from '@sveltejs/kit';
+import type { LayoutLoad } from './$types';
+import { PUBLIC_API_URL } from '$env/static/public';
+
+
+export const ssr = false;
+
+export const load = (async ({ }) => {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+	const res = await fetch(PUBLIC_API_URL+'/account', {
+		method: 'GET',
+		credentials: 'include',
+		headers
+	})
+	if(res.status == 200){
+		const data = await res.json()
+		if(data.user.role != 'admin'){
+			error(403, {
+				message: 'Forbidden',
+			});
+		}
+	}
+	if(res.status == 401){
+		error(403, {
+			message: 'Forbidden',
+		});
+	}
+}) satisfies LayoutLoad;
